b72a39721237f70b54983cb5847de1ffb2d317ce
adrianxms <adriangabriel.fo1@gmail.com>
2024-07-12 20:43:14 -0300

    versão final

--- src/entities/Program.java
+++ src/entities/Program.java
@@ -1,14 +1,14 @@
 package entities;
 
 public class Program {
-    public int accountNumber;
+    public int accountNumber; // mudar depois o modificador de acesso.
     public String name;
     private double balance;
 
     public Program(int accountNumber, String name, double firstbalance) {
         this.accountNumber = accountNumber;
         this.name = name;
-        balance = firstbalance;
+        Deposit(firstbalance);
     }
     public Program(int accountNumber, String name){
         this.accountNumber = accountNumber;
@@ -25,21 +25,17 @@ public class Program {
         this.accountNumber = accountNumber;
     }
 
-    public String getName(){
-        return this.name;
-    }
-
-    public void setName(String Name){
-        this.name = name;
-    }
-
     public void Deposit(double depositAmount){
     balance += depositAmount;
     }
 
     public void Withdraw(double WithdrawAmount){
-        balance -= WithdrawAmount;
-        balance -= 5;
+        if(WithdrawAmount <= (balance - 5)) {
+            balance -= WithdrawAmount;
+            balance -= 5;
+        }else{
+            System.out.println("Saldo Insuficiente, considere o valor da taxa de saque.");
+        }
     }
 
     public String toString(){
